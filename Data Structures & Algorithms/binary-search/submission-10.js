cde3566f654ca11c52f4b9a78a3b63c0df03b2b3
Abhi K Tiwari <46610829+KayTiwari@users.noreply.github.com>
2026-05-19 10:13:31 -0400

    Add: binary-search - submission-10

---
+++ Data Structures & Algorithms/binary-search/submission-10.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+       let l = Math.floor(nums.length/2)
+       while (l > -2) {
+        console.log(nums[l] > target && !(nums[l - 1] < target))
+            if (nums[l] === target) {
+                return l;
+            }
+            else if (nums[l] < target && !(nums[l + 1] > target)) {
+                l++;
+            }
+            else if (nums[l] > target && !(nums[l - 1] < target)) {
+                l--;
+            }
+            else return -1
+       }
+    }
+}
